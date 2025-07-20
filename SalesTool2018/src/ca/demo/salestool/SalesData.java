968cb460b71a829b073aa79f7086809378ccd073
shamikroyc15 <shamikroyc15@gmail.com>
2025-07-20 18:26:16 -0400

    Added Code for Multiplication

--- SalesTool2018/src/ca/demo/salestool/SalesData.java
+++ SalesTool2018/src/ca/demo/salestool/SalesData.java
@@ -7,7 +7,14 @@ public class SalesData {
 		System.out.println("Data in the Array: ");
 		for(int i: data)	
 			System.out.println("Next Value of Array Data = : " + i);
-	
+		
+		// Adding code for multiply 
+		double multiply = 1;
+		for(int i = 0; i < data.length; i++) {
+			multiply = multiply * data[i];
+		}
+		
+		System.out.println("Multiply Value = " + multiply);
 	}
 
 }
