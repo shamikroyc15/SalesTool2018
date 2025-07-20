837f26412bec3802b38145ad76dc64a67ed457d5
shamikroyc15 <shamikroyc15@gmail.com>
2025-07-20 18:06:05 -0400

    Updated main.java + SalesData.java

--- SalesTool2018/src/ca/demo/salestool/SalesData.java
+++ SalesTool2018/src/ca/demo/salestool/SalesData.java
@@ -1,13 +1,13 @@
 package ca.demo.salestool;
 
 public class SalesData {
-	private int data[] = {0, 4, 42};
+	private int data[] = {-1, 0, 4, 42, 84};
 
 	public void display() {
-		System.out.println("Data: ");
-		for(int i = 0; i < data.length; i++) {
-			System.out.println("Next Value: + data");
-		}
+		System.out.println("Data in the Array: ");
+		for(int i: data)	
+			System.out.println("Next Value of Array Data = : " + i);
+	
 	}
 
 }
