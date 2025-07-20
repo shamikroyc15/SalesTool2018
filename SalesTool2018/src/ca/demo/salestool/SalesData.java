890a337d7dc9897c8c9fcacb0d8d134147b5cec4
shamikroyc15 <shamikroyc15@gmail.com>
2025-07-20 18:01:41 -0400

    Update SalesData.java - Summed Up Values during display + Added Class Comment

--- SalesTool2018/src/ca/demo/salestool/SalesData.java
+++ SalesTool2018/src/ca/demo/salestool/SalesData.java
@@ -1,10 +1,20 @@
 package ca.demo.salestool;
 
+/*
+    Represent the sales data for a given period 
+*/
 public class SalesData {
 	private int data[] = {0, 4, 42};
 
 	public void display() {
-		System.out.println("TODO: Implement data display");
+		System.out.println("Data: ");
+		int sum = 0;
+		for(int i: data) {
+			System.out.println("Next Value: " + i);
+			sum += i;
+		}
+
+		System.out.println("Sum is: " + sum);
 	}
 
 }
