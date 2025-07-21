138d9fbd561bce26c3c11f5867e4241a7bf0917b
shamikroyc15 <shamikroyc15@gmail.com>
2025-07-20 20:16:44 -0400

    Stashed Popped changes + Resolved Merge Conflicts

--- SalesTool2018/src/ca/demo/salestool/Main.java
+++ SalesTool2018/src/ca/demo/salestool/Main.java
@@ -1,4 +1,7 @@
 package ca.demo.salestool;
+
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -14,9 +17,22 @@ public class Main {
 		System.out.println("WOot! It pushed!\n");
 	}
 	
-	public static void stringReversal() {
+	
+	private static void doStashDemo1() {
 		
-		String originalStr = "Hello";
+		int x, y, sum;
+	    Scanner myObj = new Scanner(System.in); // Create a Scanner object
+	    System.out.println("Type a number:");
+	    x = myObj.nextInt(); // Read user input
+
+	    System.out.println("Type another number:");
+	    y = myObj.nextInt(); // Read user input
+
+	    sum = x + y;  // Calculate the sum of x + y
+	    System.out.println("Sum is: " + sum); // Print the sum
+	    
+	    // String Reversal 
+	    String originalStr = "Hello";
 		String reversedStr = "";
 
 		for (int i = 0; i < originalStr.length(); i++) {
@@ -24,6 +40,8 @@ public class Main {
 		}
 
 		System.out.println("Reversed string: "+ reversedStr);
+		
 	}
+	
 
 }
