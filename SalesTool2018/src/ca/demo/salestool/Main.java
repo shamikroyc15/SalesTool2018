d00567bcd5e1a0103cb9ec340604bf492c81f158
shamikroyc15 <shamikroyc15@gmail.com>
2025-07-20 20:13:47 -0400

    Stashed Popped data back to main.java

--- SalesTool2018/src/ca/demo/salestool/Main.java
+++ SalesTool2018/src/ca/demo/salestool/Main.java
@@ -13,5 +13,17 @@ public class Main {
 		System.out.println("THIS APP SHOWS SALES DATA");
 		System.out.println("WOot! It pushed!\n");
 	}
+	
+	public static void stringReversal() {
+		
+		String originalStr = "Hello";
+		String reversedStr = "";
+
+		for (int i = 0; i < originalStr.length(); i++) {
+		  reversedStr = originalStr.charAt(i) + reversedStr;
+		}
+
+		System.out.println("Reversed string: "+ reversedStr);
+	}
 
 }
